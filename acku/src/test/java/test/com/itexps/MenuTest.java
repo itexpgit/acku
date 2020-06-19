ccf8f2c633d31c33c4aeef3783fa212a11aebb30
itexpgit <66497358+itexpgit@users.noreply.github.com>
2020-06-19 12:37:11 -0500

    updates 21

--- acku/src/test/java/test/com/itexps/MenuTest.java
+++ acku/src/test/java/test/com/itexps/MenuTest.java
@@ -79,6 +79,20 @@ public class MenuTest {
         wait.until(ExpectedConditions.urlToBe("https://bamboo-gardens.com/wp-content/uploads/2019/03/SampleMenu-Mar2019.pdf"));
         Thread.sleep(2000);
         System.out.println("Sample Menu Validation completed");
+        driver.close();
+        Thread.sleep(2000);
+        driver.switchTo().window(tabs.get(0));
+        driver.switchTo().activeElement().sendKeys(Keys.TAB);
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"menu-content\"]/a[2]")).click();
+        System.out.println("Craft Cocktail Menu Opened");
+        ArrayList<String> tabs2 = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(tabs2.get(1));
+        System.out.println("Focus shifted to second tab");
+        wait.until(ExpectedConditions.urlToBe("https://bamboo-gardens.com/wp-content/uploads/2019/03/CraftCocktail_SampleMenu.pdf"));
+        System.out.println("Craft Cocktail Validation completed");
+        Thread.sleep(2000);
+        driver.close();
        
 
     }
