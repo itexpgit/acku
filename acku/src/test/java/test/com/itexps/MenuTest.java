b5186e6e7e107f4f5d8c2c009b0bddf59a87753f
itexpgit <66497358+itexpgit@users.noreply.github.com>
2020-06-19 15:07:27 -0500

    updates 35

--- acku/src/test/java/test/com/itexps/MenuTest.java
+++ acku/src/test/java/test/com/itexps/MenuTest.java
@@ -66,12 +66,9 @@ public class MenuTest {
         driver.manage().window().maximize();
         driver.get(baseUrl);
         System.out.println("Loaded Bamboo Garden website");
-        Thread.sleep(1000);
         driver.findElement(By.cssSelector("[title=\"Menu\"]")).click();
-        Thread.sleep(2000);
         System.out.println("Selected Menu");
         driver.findElement(By.xpath("//*[@id=\"menu-content\"]/a[1]")).click();
-        Thread.sleep(1000);
         System.out.println("Selected Sample Menu");
         int winCount;
         do {
@@ -79,20 +76,16 @@ public class MenuTest {
             winCount = allWindwHandles.size();
             Thread.sleep(1000);
         }
-        
         while(winCount !=2);
         ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
         driver.switchTo().window(tabs.get(1));
         System.out.println("Focus shifted to second tab");
         WebDriverWait wait = new WebDriverWait(driver, 30);
         wait.until(ExpectedConditions.urlToBe("https://bamboo-gardens.com/wp-content/uploads/2019/03/SampleMenu-Mar2019.pdf"));
-        Thread.sleep(2000);
         System.out.println("Sample Menu Validation completed");
         driver.close();
-        Thread.sleep(2000);
         driver.switchTo().window(tabs.get(0));
         driver.switchTo().activeElement().sendKeys(Keys.TAB);
-        Thread.sleep(2000);
         driver.findElement(By.xpath("//*[@id=\"menu-content\"]/a[2]")).click();
         System.out.println("Craft Cocktail Menu Opened");
         int winCount1;
@@ -101,18 +94,15 @@ public class MenuTest {
             winCount1 = allWindwHandles1.size();
             Thread.sleep(1000);
         }
-        
         while(winCount1 !=2);
         ArrayList<String> tabs2 = new ArrayList<String>(driver.getWindowHandles());
         driver.switchTo().window(tabs2.get(1));
         System.out.println("Focus shifted to second tab");
         wait.until(ExpectedConditions.urlToBe("https://bamboo-gardens.com/wp-content/uploads/2019/03/CraftCocktail_SampleMenu.pdf"));
         System.out.println("Craft Cocktail Validation completed");
-        Thread.sleep(2000);
         driver.close();
         driver.switchTo().window(tabs.get(0));
         driver.switchTo().activeElement().sendKeys(Keys.TAB);
-        Thread.sleep(2000);
         driver.findElement(By.xpath("//*[@id=\"menu-content\"]/a[3]")).click();
         System.out.println("Selected Order Online");
         int winCount2;
@@ -121,38 +111,28 @@ public class MenuTest {
             winCount2 = allWindwHandles2.size();
             Thread.sleep(1000);
         }
-        
         while(winCount2 !=2);
         ArrayList<String> tabs3 = new ArrayList<String>(driver.getWindowHandles());
         driver.switchTo().window(tabs3.get(1));
         wait.until(ExpectedConditions.urlToBe("https://www.inchinsonline.com/products"));
         System.out.println("Inchinsonline order website opened");
-        Thread.sleep(2000);
         driver.findElement(By.linkText("Select This Location")).click();
         System.out.println("Chicago location selected");
-        Thread.sleep(2000);
         driver.findElement(By.xpath("//div[@id='price-list-0']/ul/li/h5")).click();
         System.out.println("Main Menu selected");
-        Thread.sleep(2000);
-        driver.findElement(By.linkText("Login")).click();
+        driver.findElement(By.xpath("//*[@id=\"navbar\"]/ul[2]/li[2]/a")).click();
         System.out.println("Login Selected");
-        Thread.sleep(1000);
         driver.findElement(By.id("email")).click();
         System.out.println("Email Selected");
-        Thread.sleep(1000);
         driver.findElement(By.name("email")).sendKeys("ackuselenium@gmail.com");
         System.out.println("Email Entered");
-        Thread.sleep(1000);
         driver.findElement(By.name("password")).click();
         System.out.println("Password Selected");
-        Thread.sleep(1000);
         driver.findElement(By.name("password")).sendKeys("itexperts");
         System.out.println("Password Entered");
-        Thread.sleep(1000);
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         System.out.println("Login process completed");
-        Thread.sleep(1000);
-       
+            
         driver.close();
 
         
