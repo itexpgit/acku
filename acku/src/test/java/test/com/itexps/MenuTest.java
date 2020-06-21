9284959a350e924956dba7f174da83aa7df54a58
itexpgit <66497358+itexpgit@users.noreply.github.com>
2020-06-20 21:38:01 -0500

    upload 116

--- acku/src/test/java/test/com/itexps/MenuTest.java
+++ acku/src/test/java/test/com/itexps/MenuTest.java
@@ -70,12 +70,8 @@ public class MenuTest {
         
         driver.manage().window().maximize();
         driver.get(baseUrl);
-        driver.findElement(By.id("email")).click();
-        System.out.println("Email field selected");
         driver.findElement(By.name("email")).sendKeys("ackuselenium5@gmail.com");
         System.out.println("Email Entered");
-        driver.findElement(By.name("password")).click();
-        System.out.println("Password field selected");
         driver.findElement(By.name("password")).sendKeys("itexperts");
         System.out.println("Password entered");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
@@ -111,10 +107,8 @@ public class MenuTest {
         driver.switchTo().activeElement().sendKeys(Keys.TAB);
         driver.switchTo().activeElement().sendKeys(Keys.ENTER);
         System.out.println("Check out process started");
-        driver.findElement(By.cssSelector("input[name=\"ccNumber\"]")).click();
-        driver.findElement(By.cssSelector("input[name=\"ccNumber\"]")).clear();
         Thread.sleep(1000);
-        driver.findElement(By.cssSelector("input[name=\"ccNumber\"]")).sendKeys("234567891234");
+        driver.findElement(By.name("ccNumber")).sendKeys("234567891234");
         System.out.println("Credit Card number entered");
         /*Thread.sleep(1000);
         driver.findElement(By.name("ccExpiry")).click();
