bcc3fbd7ef1ebb5bf011e260465c486a4cc2b0cd
itexpgit <66497358+itexpgit@users.noreply.github.com>
2020-06-21 14:58:47 -0500

    update 103

--- acku/src/test/java/test/com/itexps/MenuTest.java
+++ acku/src/test/java/test/com/itexps/MenuTest.java
@@ -52,6 +52,7 @@ public class MenuTest {
         System.setProperty("webdriver.chrome.driver", "c:\\data\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--incognito");
+        options.addArguments("--window-size=1920,1080");
         options.addArguments("--start-maximized");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
@@ -77,26 +78,48 @@ public class MenuTest {
         System.out.println("Password entered");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         System.out.println("Login completed");
-        Thread.sleep(5000);
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@id=\"location-list\"]/li[1]/h4/a")).click();
         System.out.println("Chicago location selected");
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//div[@id='price-list-0']/ul/li/h5")).click();
         System.out.println("Main Menu selected");
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@id=\"all-categories\"]/ul/li[6]/a")).click();
         System.out.println("Clicked Noodles Section");
         Thread.sleep(2000);
         driver.findElement(By.xpath("//img[@src='https://s3-us-west-2.amazonaws.com/www.gethomesome.com/productimages/ibg-noodles-hakka-mod.jpg']")).click();
         System.out.println("Selected Hakka Noodles");
-        Thread.sleep(2000);
         driver.findElement(By.id("update-btn")).click();
+        System.out.println("Item Added to Cart");
         Thread.sleep(2000);
-        System.out.println("Added to cart");
+        driver.findElement(By.xpath("//*[@id=\"navbar\"]/ul[2]/li[7]/a/span[2]")).click();
+        System.out.println("Cart Selected");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("/html/body/div[2]/section/div/div/div[3]/div[2]/div/a")).click();
+        System.out.println("Checkout Selected");
+        Thread.sleep(1000);
+        driver.findElement(By.name("ccNumber")).sendKeys("234567891234");
+        System.out.println("Credit Card Number Entered");
+        Thread.sleep(1000);
+        driver.findElement(By.name("ccExpiry")).sendKeys("10/23");
+        System.out.println("Credit Card Expiry Entered");
+        Thread.sleep(1000);
+        driver.findElement(By.name("ccCvc")).sendKeys("203");
+        System.out.println("Credit Card CVV Number Entered");
+        Thread.sleep(1000);
+        driver.findElement(By.name("phoneUS")).sendKeys("4029067731");
+        System.out.println("Phone Number Entered");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[2]/section/div/div/div[4]/div[2]/div/ul/li[7]/div/a")).click();
+        System.out.println("Order Placed");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//b[@class='caret']")).click();
+        System.out.println("ACKU Menu Clicked");
+        driver.findElement(By.xpath("//*[@id=\"navbar\"]/ul[2]/li[6]/ul/li[4]/a")).click();
+        System.out.println("Logout completed");
         driver.close();
     }
-				
-
+	
 }
-
 
